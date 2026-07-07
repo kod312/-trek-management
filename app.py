3a51cc2e66d212e624566152d4e4a25bea1e9064
nandh <shalu032006@gmail.com>
2026-07-07 08:50:53 +0530

    Add User, Trek, Booking models

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
  
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models import db, User, Trek, Booking
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trek.db'
+db.init_app(app)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        print("Tables created!")
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,1 +1,44 @@
  
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(100))
+    contact = db.Column(db.String(20))
+    is_blacklisted = db.Column(db.Boolean, default=False)
+class Trek(db.Model):
+	__tablename__ = "trek"
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80), unique=True, nullable=False)
+	location= db.Column(db.String(200), nullable=False)	
+	difficulty= db.Column(db.String(20), nullable=False)
+	duration=db.Column(db.Integer)
+	available_slots=db.Column(db.Integer)
+	status=db.Column(db.String(20), nullable=False)
+	start_date=db.Column(db.String(20))
+	end_date=db.Column(db.String(20))
+	assigned_staff_id=db.Column(db.Integer, db.ForeignKey("user.id"))
+class Booking(db.Model):
+	__tablename__="booking"
+	id = db.Column(db.Integer, primary_key=True)
+	user_id=db.Column(db.Integer, db.ForeignKey("user.id"))
+	trek_id=db.Column(db.Integer, db.ForeignKey("trek.id"))
+	booking_date=db.Column(db.String(20))
+	status=db.Column(db.String(20))
+
+
+
+
+
+
+
+	
+	
